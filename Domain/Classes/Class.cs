c9654fd2976e8554b13eeeee1d2f128617e503e6
Ali Noorkazemi <alinoorkazemi@gmail.com>
2023-06-25 17:02:28 +0330

    Re-add project template

---
+++ Domain/Classes/Class.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain.Classes;
+
+public class Class
+{
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string TeacherId { get; set; }
+
+    public Class(string name, string teacherId)
+    {
+        Name = name;
+        TeacherId = teacherId;
+    }
+
+    public void Update(string name, string teacherId)
+    {
+        Name = name;
+        TeacherId = teacherId;
+    }
+}
---
+++ Domain/Lessons/Lesson.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Lessons;
+
+public class Lesson
+{
+    public Lesson(string note, string mediaId, string classId)
+    {
+        Note = note;
+        MediaId = mediaId;
+        ClassId = classId;
+    }
+
+    public string Id { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public string Note { get; set; }
+
+    public string MediaId { get; set; }
+
+    public string ClassId { get; set; }
+}
