f9c63b05d34345f3031bce28da66b3514cae795a
AshkanFam <ashkanfamoorian1379@gmail.com>
2023-06-26 00:56:21 +0430

    Add Users handlers

---
+++ Application/Handlers/Users/Students/CreateStudentHandler.cs
@@ -0,0 +1,8 @@
+﻿using Application.Commands.Users.Students;
+using LiteBus.Commands.Abstractions;
+
+namespace Application.Handlers.Users.Students;
+
+public class CreateStudentHandler : ICommandHandler<CreateStudentCommand, string>
+{
+}
---
+++ Application/Handlers/Users/Teachers/DeleteTeacherHandler.cs
@@ -0,0 +1,9 @@
+﻿using Application.Commands.Users.Teachers;
+using LiteBus.Commands.Abstractions;
+
+namespace Application.Handlers.Users.Teachers;
+
+public class DeleteTeacherHandler : ICommandHandler<DeleteTeacherCommand>
+{
+    
+}
