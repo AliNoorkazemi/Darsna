d517fab677358535484ba1bc15953e4f1077aa02
AliNoorkazemi <alinoorkazemi@gmail.com>
2023-06-25 21:06:12 +0330

    Add Student domain

---
+++ Domain/Users/Students/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Users.Students;
+
+public class Student : User
+{
+    public IReadOnlyCollection<string> FavoriteClassIds
+    {
+        get => _favoriteClassIds;
+        set => _favoriteClassIds = new SortedSet<string>(value);
+    }
+
+    private SortedSet<string> _favoriteClassIds = new();
+
+    public Student(string userName,
+        string email,
+        string password,
+        string name,
+        IEnumerable<string> favoriteClassIds)
+        : base(userName,
+            email,
+            password,
+            name)
+    {
+        FavoriteClassIds = favoriteClassIds.ToList();
+    }
+}
