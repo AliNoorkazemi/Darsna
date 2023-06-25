33bd77c8266b02c643bfb97128ac6e4c4ac4b405
AliNoorkazemi <alinoorkazemi@gmail.com>
2023-06-25 21:06:21 +0330

    Add Teacher domain

---
+++ Domain/Users/Teachers/Teacher.cs
@@ -0,0 +1,25 @@
+﻿namespace Domain.Users.Teachers;
+
+public class Teacher : User
+{
+    public IReadOnlyCollection<string> ClassIds
+    {
+        get  => _classIds; 
+        set => _classIds = new SortedSet<string>(value);
+    }
+
+    private SortedSet<string> _classIds = new();
+
+    public Teacher(string userName, 
+        string email, 
+        string password, 
+        string name,
+        IEnumerable<string> classIds) 
+        : base(userName, 
+            email, 
+            password, 
+            name)
+    {
+        ClassIds = classIds.ToList();
+    }
+}
