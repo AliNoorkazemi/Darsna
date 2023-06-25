9ccab0139c4f3942c410cc00337f133ecf472e18
AshkanFam <ashkanfamoorian1379@gmail.com>
2023-06-26 00:49:57 +0430

    Add Lessons commands

---
+++ Application/Commands/Lessons/CreateLessonCommand.cs
@@ -0,0 +1,12 @@
+﻿using LiteBus.Commands.Abstractions;
+
+namespace Application.Commands.Lessons;
+
+public class CreateLessonCommand : ICommand<string>
+{
+    public string Note { get; set; }
+
+    public string MediaId { get; set; }
+
+    public string ClassId { get; set; }
+}
---
+++ Application/Commands/Lessons/DeleteLessonCommand.cs
@@ -0,0 +1,8 @@
+﻿using LiteBus.Commands.Abstractions;
+
+namespace Application.Commands.Lessons;
+
+public class DeleteLessonCommand : ICommand
+{
+    public string Id { get; set; }
+}
