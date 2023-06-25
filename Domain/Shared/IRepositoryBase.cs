0c678c81aaf5059087c200544c401e66fb699f6f
AliNoorkazemi <alinoorkazemi@gmail.com>
2023-06-25 21:19:17 +0330

    Add repository base interface

---
+++ Domain/Shared/IRepositoryBase.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Shared;
+
+public interface IRepositoryBase<T, TKey>
+{
+    Task<TKey> AddAsync(T domain);
+
+    Task UpdateAsync(T domain);
+
+    Task DeleteAsync(T domain);
+
+    Task<T> GetByIdAsync(TKey key);
+}
