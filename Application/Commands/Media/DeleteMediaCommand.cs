4d98fca1bc83149ac31d2a9cf3db07d9ca4160c4
AshkanFam <ashkanfamoorian1379@gmail.com>
2023-06-26 00:50:59 +0430

    Add Media commands

---
+++ Application/Commands/Media/DeleteMediaCommand.cs
@@ -0,0 +1,8 @@
+﻿using LiteBus.Commands.Abstractions;
+
+namespace Application.Commands.Media;
+
+public class DeleteMediaCommand : ICommand
+{
+    public string Id { get; set; }
+}
---
+++ Application/Commands/Media/UpdateMediaCommand.cs
@@ -0,0 +1,14 @@
+﻿using LiteBus.Commands.Abstractions;
+
+namespace Application.Commands.Media;
+
+public class UpdateMediaCommand : ICommand
+{
+    public string Id { get; set; }
+
+    public string Url { get; set; }
+
+    public int Size { get; set; }
+
+    public long DurationTicks { get; set; }
+}
