b641e47545fac3ae494b948c5f46c8b7528d3a35
Ali Noorkazemi <alinoorkazemi@gmail.com>
2023-06-25 16:25:20 +0330

    Add Class domain

---
+++ Domain/Classes/Class.cs
@@ -0,0 +1,23 @@
+﻿namespace Domain.Classes;
+
+public class Class
+{
+    public string Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string TeacherId { get; set; }
+
+    public Class(string id, string name, string teacherId)
+    {
+        Id = id;
+        Name = name;
+        TeacherId = teacherId;
+    }
+
+    public void Update(string name, string teacherId)
+    {
+        Name = name;
+        TeacherId = teacherId;
+    }
+}
