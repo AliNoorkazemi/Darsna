5644dd57e27ba0e0243e623902e9a5635cb22ea2
AshkanFam <ashkanfamoorian1379@gmail.com>
2023-06-26 00:55:08 +0430

    Add Lessons handlers

---
+++ Application/Handlers/Lessons/CreateLessonHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Commands.Lessons;
+using Domain.Lessons;
+using LiteBus.Commands.Abstractions;
+
+namespace Application.Handlers.Lessons;
+
+public class CreateLessonHandler : ICommandHandler<CreateLessonCommand, string>
+{
+    private readonly ILessonRepository _repository;
+
+    public CreateLessonHandler(ILessonRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<string> HandleAsync(CreateLessonCommand message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var domain = new Lesson(message.Note, message.MediaId, message.ClassId);
+    }
+}
