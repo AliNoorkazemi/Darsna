a2c8244ebd72ee54c98d3fe3618aec2807a00c10
AliNoorkazemi <alinoorkazemi@gmail.com>
2023-06-25 21:06:01 +0330

    Add base User domain

--- Domain/Users/User.cs
+++ Domain/Users/User.cs
@@ -1,6 +1,22 @@
 ﻿namespace Domain.Users;
 
-public class User
+public abstract class User
 {
-    
+    public string _id { get; set; }
+
+    public string UserName { get; set; }
+
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public string Name { get; set; }
+
+    protected User(string userName, string email, string password, string name)
+    {
+        UserName = userName;
+        Email = email;
+        Password = password;
+        Name = name;
+    }
 }
