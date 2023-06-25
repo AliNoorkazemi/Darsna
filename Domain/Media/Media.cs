cba7e51638354f4724537a5f2ad1e2839d678e32
AliNoorkazemi <alinoorkazemi@gmail.com>
2023-06-25 21:05:49 +0330

    Add Media domain

--- Domain/Media/Media.cs
+++ Domain/Media/Media.cs
@@ -2,5 +2,21 @@
 
 public class Media
 {
-    
+    public string Id { get; set; }
+
+    public string Url { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public int Size { get; set; }
+
+    public long DurationTicks { get; set; }
+
+    public Media(string url, int size, long durationTicks)
+    {
+        Url = url;
+        Size = size;
+        DurationTicks = durationTicks;
+        CreatedAt = DateTime.Now;
+    }
 }
