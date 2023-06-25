815cc97f4f729adea30b0b868221d75c9231f16c
AshkanFam <ashkanfamoorian1379@gmail.com>
2023-06-26 00:48:33 +0430

    Add Class commands

---
+++ Application/Commands/Classes/CreateClassCommand.cs
@@ -0,0 +1,10 @@
+﻿using LiteBus.Commands.Abstractions;
+
+namespace Application.Commands.Classes;
+
+public class CreateClassCommand : ICommand<string>
+{
+    public string Name { get; set; }
+
+    public string TeacherId { get; set; }
+}
---
+++ Application/Commands/Classes/DeleteClassCommand.cs
@@ -0,0 +1,8 @@
+﻿using LiteBus.Commands.Abstractions;
+
+namespace Application.Commands.Classes;
+
+public class DeleteClassCommand : ICommand
+{
+    public string Id { get; set; }
+}
