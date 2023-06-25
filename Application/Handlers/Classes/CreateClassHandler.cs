38cfbee1579196c3dcdca55d830270b1be0626fd
AshkanFam <ashkanfamoorian1379@gmail.com>
2023-06-26 00:54:21 +0430

    Add Classes handlers

---
+++ Application/Handlers/Classes/CreateClassHandler.cs
@@ -0,0 +1,23 @@
+﻿using Application.Commands.Classes;
+using Domain.Classes;
+using LiteBus.Commands.Abstractions;
+
+namespace Application.Handlers.Classes;
+
+public class CreateClassHandler : ICommandHandler<CreateClassCommand, string>
+{
+    private readonly IClassRepository _repository;
+
+    public CreateClassHandler(IClassRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<string> HandleAsync(CreateClassCommand message,
+        CancellationToken cancellationToken = new CancellationToken())
+    {
+        var domain = new Class(message.Name, message.TeacherId);
+
+        return _repository.AddAsync(domain);
+    }
+}
