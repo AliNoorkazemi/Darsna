def48cacfd96ba1961dd0902ee93073a67c09bde
miganmht <miganmht@gmail.com>
2023-06-26 10:49:03 +0330

    add classes handlers

---
+++ Query/Handlers/Classes/GetClassByIdHandler.cs
@@ -0,0 +1,13 @@
+﻿using LiteBus.Queries.Abstractions;
+using Query.Queries.Classes;
+using Query.QueryModels.Classes;
+
+namespace Query.Handlers.Classes;
+
+public class GetClassByIdHandler : IQueryHandler<GetClassByIdQuery, ClassQueryModel>
+{
+    public Task<ClassQueryModel> HandleAsync(GetClassByIdQuery message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+}
