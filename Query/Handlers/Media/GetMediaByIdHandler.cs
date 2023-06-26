42683754982aa52cb3c8aee78ac8c3a0d33a69ba
miganmht <miganmht@gmail.com>
2023-06-26 10:50:57 +0330

    add Media handlers

---
+++ Query/Handlers/Media/GetMediaByIdHandler.cs
@@ -0,0 +1,13 @@
+﻿using LiteBus.Queries.Abstractions;
+using Query.Queries.Media;
+using Query.QueryModels.Media;
+
+namespace Query.Handlers.Media;
+
+public class GetMediaByIdHandler : IQueryHandler<GetMediaByIdQuery, MediaQueryModel>
+{
+    public Task<MediaQueryModel> HandleAsync(GetMediaByIdQuery message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Query/Handlers/Media/GetMediaHandler.cs
@@ -0,0 +1,13 @@
+﻿using LiteBus.Queries.Abstractions;
+using Query.Queries.Media;
+using Query.QueryModels.Media;
+
+namespace Query.Handlers.Media;
+
+public class GetMediaHandler : IQueryHandler<GetMediaQuery, IReadOnlyCollection<MediaListQueryModel>>
+{
+    public Task<IReadOnlyCollection<MediaListQueryModel>> HandleAsync(GetMediaQuery message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+}
