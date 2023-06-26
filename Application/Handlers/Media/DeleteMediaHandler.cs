d904c0d53897d4fc84041af2fe96af03b5eb863b
Ali Noorkazemi <alinoorkazemi@gmail.com>
2023-06-26 09:17:06 +0330

    Complete handlers in Media application

--- Application/Handlers/Media/DeleteMediaHandler.cs
+++ Application/Handlers/Media/DeleteMediaHandler.cs
@@ -1,8 +1,22 @@
 ﻿using Application.Commands.Media;
+using Domain.Media;
 using LiteBus.Commands.Abstractions;
 
 namespace Application.Handlers.Media;
 
 public class DeleteMediaHandler : ICommandHandler<DeleteMediaCommand>
 {
+    private readonly IMediaRepository _repository;
+
+    public DeleteMediaHandler(IMediaRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task HandleAsync(DeleteMediaCommand message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var domain = await _repository.GetByIdAsync(message.Id);
+
+        await _repository.DeleteAsync(domain);
+    }
 }
