218125915184ebbd713bc803978dc938a090d484
miganmht <miganmht@gmail.com>
2023-06-26 10:51:40 +0330

    add User Handlers

---
+++ Query/Handlers/Users/Students/GetStudentsHandler.cs
@@ -0,0 +1,13 @@
+﻿using LiteBus.Queries.Abstractions;
+using Query.Queries.Users.Students;
+using Query.QueryModels.Users.Students;
+
+namespace Query.Handlers.Users.Students;
+
+public class GetStudentsHandler : IQueryHandler<GetStudentsQuery, IReadOnlyCollection<StudentListQueryModel>>
+{
+    public Task<IReadOnlyCollection<StudentListQueryModel>> HandleAsync(GetStudentsQuery message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Query/Handlers/Users/Teachers/GetTeachersHandler.cs
@@ -0,0 +1,13 @@
+﻿using LiteBus.Queries.Abstractions;
+using Query.Queries.Users.Teachers;
+using Query.QueryModels.Users.Teachers;
+
+namespace Query.Handlers.Users.Teachers;
+
+public class GetTeachersHandler : IQueryHandler<GetTeachersQuery, IReadOnlyCollection<TeacherListQueryModel>>
+{
+    public Task<IReadOnlyCollection<TeacherListQueryModel>> HandleAsync(GetTeachersQuery message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        throw new NotImplementedException();
+    }
+}
