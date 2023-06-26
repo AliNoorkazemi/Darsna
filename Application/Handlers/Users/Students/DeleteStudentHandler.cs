84359b3ed7875c8c32e4ce86c1851c4c1e9d06cf
Ali Noorkazemi <alinoorkazemi@gmail.com>
2023-06-26 09:27:56 +0330

    Complete student handlers in application layer

--- Application/Handlers/Users/Students/DeleteStudentHandler.cs
+++ Application/Handlers/Users/Students/DeleteStudentHandler.cs
@@ -1,9 +1,22 @@
 ﻿using Application.Commands.Users.Students;
+using Domain.Users;
 using LiteBus.Commands.Abstractions;
 
 namespace Application.Handlers.Users.Students;
 
 public class DeleteStudentHandler : ICommandHandler<DeleteStudentCommand>
 {
-    
+    private readonly IUserRepository _repository;
+
+    public DeleteStudentHandler(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task HandleAsync(DeleteStudentCommand message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var domain = await _repository.GetByIdAsync(message.Id);
+
+        await _repository.DeleteAsync(domain);
+    }
 }
--- Application/Handlers/Users/Students/UpdateStudentHandler.cs
+++ Application/Handlers/Users/Students/UpdateStudentHandler.cs
@@ -1,8 +1,22 @@
 ﻿using Application.Commands.Users.Students;
+using Domain.Users;
 using LiteBus.Commands.Abstractions;
 
 namespace Application.Handlers.Users.Students;
 
 public class UpdateStudentHandler : ICommandHandler<UpdateStudentCommand>
 {
+    private readonly IUserRepository _repository;
+
+    public UpdateStudentHandler(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task HandleAsync(UpdateStudentCommand message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var domain = await _repository.GetByIdAsync(message.Id);
+
+        await _repository.DeleteAsync(domain);
+    }
 }
