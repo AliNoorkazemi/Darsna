0f300a8596fcb735a1ca8f902db4fa2cfada2c8a
AliNoorkazemi <alinoorkazemi@gmail.com>
2023-06-26 20:26:07 +0330

    Add mediator configuration

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,3 +1,9 @@
+using Application.Handlers.Classes;
+using LiteBus.Commands.Extensions.MicrosoftDependencyInjection;
+using LiteBus.Messaging.Extensions.MicrosoftDependencyInjection;
+using LiteBus.Queries.Extensions.MicrosoftDependencyInjection;
+using Query.Handlers.Classes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddLiteBus(conf =>
+{
+    conf.AddCommands(commandBuilder => { commandBuilder.RegisterFrom(typeof(CreateClassHandler).Assembly); })
+        .AddQueries(queryBuilder => { queryBuilder.RegisterFrom(typeof(GetClassesHandler).Assembly); });
+});
 
 var app = builder.Build();
 
