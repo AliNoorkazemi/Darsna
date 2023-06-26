dafbc234bbb29db97664abf75158e2ebf3b4f4be
Ali Noorkazemi <alinoorkazemi@gmail.com>
2023-06-26 09:36:38 +0330

    Complete teacher application layer

--- Application/Handlers/Users/Teachers/CreateTeacherHandler.cs
+++ Application/Handlers/Users/Teachers/CreateTeacherHandler.cs
@@ -1,8 +1,27 @@
 ﻿using Application.Commands.Users.Teachers;
+using Domain.Users;
+using Domain.Users.Teachers;
 using LiteBus.Commands.Abstractions;
 
 namespace Application.Handlers.Users.Teachers;
 
 public class CreateTeacherHandler : ICommandHandler<CreateTeacherCommand, string>
 {
+    private readonly IUserRepository _repository;
+
+    public CreateTeacherHandler(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Task<string> HandleAsync(CreateTeacherCommand message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var domain = new Teacher(message.UserName,
+            message.Email,
+            message.Password,
+            message.Name,
+            message.ClassIds);
+
+        return _repository.AddAsync(domain);
+    }
 }
--- Application/Handlers/Users/Teachers/UpdateTeacherHandler.cs
+++ Application/Handlers/Users/Teachers/UpdateTeacherHandler.cs
@@ -1,8 +1,29 @@
 ﻿using Application.Commands.Users.Teachers;
+using Domain.Users;
+using Domain.Users.Teachers;
 using LiteBus.Commands.Abstractions;
 
 namespace Application.Handlers.Users.Teachers;
 
 public class UpdateTeacherHandler : ICommandHandler<UpdateTeacherCommand>
 {
+    private readonly IUserRepository _repository;
+
+    public UpdateTeacherHandler(IUserRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task HandleAsync(UpdateTeacherCommand message, CancellationToken cancellationToken = new CancellationToken())
+    {
+        var domain = await _repository.GetByIdAsync<Teacher>(message.Id);
+        
+        domain.Update(message.UserName, 
+            message.Email,
+            message.Password,
+            message.Name,
+            message.ClassIds);
+
+        await _repository.UpdateAsync(domain);
+    }
 }
